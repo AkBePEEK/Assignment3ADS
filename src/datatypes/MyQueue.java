103800dcb38ea3d44e03154ca4c03bdebdd518e2
AkBePEEK <231338@astanait.edu.kz>
2024-04-27 11:40:27 +0500

    sixth

--- src/datatypes/MyQueue.java
+++ src/datatypes/MyQueue.java
@@ -1,4 +1,33 @@
 package datatypes;
 
-public class MyQueue {
+import list.MyList;
+
+public class MyQueue<T extends Comparable<T>> {
+    private final MyList<T> list;
+
+    public MyQueue(MyList<T> list) {
+        this.list = list;
+    }
+
+    public boolean empty() {
+        return size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public T peek() {
+        return list.getLast();
+    }
+
+    public void enqueue(T t) {
+        list.addFirst(t);
+    }
+
+    public T dequeue() {
+        T t = list.getLast();
+        list.removeLast();
+        return t;
+    }
 }
